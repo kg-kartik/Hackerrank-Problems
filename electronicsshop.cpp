b83597f75118a286fbd0fe8326b05c2be1543cd4
Kartik Goel <goel.kartik39@gmail.com>
2020-03-23 18:59:59 +0530

    Updated correct solution

--- electronicsshop.cpp
+++ electronicsshop.cpp
@@ -1,11 +1,10 @@
 #include<iostream.h>
-#include<conio.h>
-int main()
-{
-    int b=0,n=0,m=0,i,j,sum=0,count=0,temp=0,counter=0,minimum=0;
+#include<algorithm.h>
+int main(){
+    int b,n,m,i,j,result=-1;
     cin>>b>>n>>m;
-    int arr1[n],arr2[m];
-    int arr[m*n];
+    int arr1[100],arr2[100],sum[100];
+
     for(i=0;i<n;i++)
     {
         cin>>arr1[i];
@@ -14,52 +13,24 @@ int main()
     {
         cin>>arr2[j];
     }
+
+    int k=0;
     for(i=0;i<n;i++)
     {
         for(j=0;j<m;j++)
         {
-            sum = arr1[i]+arr2[j];
-            count++;
+            sum[k++] = arr1[i] + arr2[j];
         }
-        arr[count] = sum;
-        sum=0;
     }
-    for(i=1;i<=count;i++)
+    
+    for(i=0;i<m*n;i++)
     {
-        for(j=i+1;j<j<=count;j++)
+        if(sum[i] <= b)
         {
-            if(arr[i] > arr[j])
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+           result = max(result,sum[i]);
         }
     }
-
-    for(i=1;i<=count;i++)
-    {
-        if(arr[i] > b)
-        {
-            counter++;
-        }
-        else
-        {
-            minimum = arr[0];
-            if((arr[i]<b) && ((b-arr[i])< minimum))
-            {
-                minimum = arr[i];
-            } 
-        }
-    }   
-    if(counter == count)
-    {
-        cout<<"-1";
-    }
-    else 
-    {
-        cout<<minimum;
-    }
+    cout<<result;   
     return 0;
 }
 
