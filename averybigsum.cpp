6da93a2864d371d3c98a663d85c090a1fe284760
Kartik Goel <goel.kartik39@gmail.com>
2020-04-09 11:33:18 +0530

    Added solution

---
+++ averybigsum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    long arr[n],sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum = sum+arr[i];
+    }
+    cout<<sum;
+}
